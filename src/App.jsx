816d733e20368bf88674c27e225febd5130750fc
PaoloRossi <paolo.rossi@pucp.edu.pe>
2022-01-11 20:42:55 -0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './styles/globals.css'
+
+function App() {
+  return (
+    <div className="w-screen h-screen grid place-content-center">
+      <div>
+        <h1>Grid Drawer</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
